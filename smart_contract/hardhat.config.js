9af99ddf0ef89695c04d42c1b0243bf5e546be84
ronak5487 <20bee055@nith.ac.in>
2023-06-29 00:40:51 +0530

    backend

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,10 @@
+// https://eth-goerli.g.alchemy.com/v2/W4bZECF0TFaI4Zw6C_kzB-LlO91xZNzE
+import '@nomicfoundation/hardhat-toolbox'
+
+export const solidity = '0.8.9'
+export const networks = {
+  goerli: {
+    url: '0x8D42E8254a7492d5f7791f8094f5F325eA787e7e',
+    accounts: ['8235540125217ce61acce148f24ccebc87859c1f606c9ba7b55a865228e74145']
+  }
+}
